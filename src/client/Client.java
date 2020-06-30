6093581da36c7caeeec455eb6a4653acb6c8a2c2
IakMastro <cse242017102@uniwa.gr>
2020-06-30 17:53:52 +0300

    Usage of fxml, deleted LoginLayout.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,70 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Client extends Application {
-    LoginLayout loginLayout;
-    ClientSocket clientSocket;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent loginScreen = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
         primaryStage.setTitle("FTP Client");
-
-        loginLayout = new LoginLayout();
-        loginLayout.getLoginButton().setOnAction(e -> loginButtonFunction());
-        loginLayout.getRegisterButton().setOnAction(e -> registerButtonFunction());
-
-        Scene loginScreen = new Scene(loginLayout);
-
-        primaryStage.setScene(loginScreen);
+        
+        Scene scene = new Scene(loginScreen, 300, 745);
+        
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    void loginButtonFunction() {
-        try {
-            clientSocket = new ClientSocket("localhost", 7500);
-            String answer = clientSocket.connect(loginLayout.getUsername(), loginLayout.getPassword());
-
-            if (answer.equals("OK")) {
-                System.out.println("I arrived back");
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Connected!");
-                alert.setContentText("Connected to the server");
-                alert.showAndWait().ifPresent(rs -> {
-                    if (rs == ButtonType.OK) {
-                        //TODO: Change the scene
-                    }
-                });
-            } else {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Wrong credentials");
-                alert.setHeaderText("Wrong credentials were given");
-                alert.setContentText("Either the user doesn't exist or the username/password is wrong.\n Try again");
-                alert.showAndWait();
-                clientSocket.logout();
-            }
-
-        } catch (IOException exception) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("There is no connection to the internet");
-            alert.setHeaderText("Server is either down or there is no internet in this machine");
-            alert.setContentText("Wrong IP address or Port was given.\nOr Server is not open.\n" +
-                    "No internet in this machine.\n Try again");
-            alert.showAndWait();
-        }
-    }
-
-    void registerButtonFunction() {
-
     }
 }
--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,4 +1,48 @@
 package client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class Controller {
+    public ClientSocket clientSocket;
+    public PasswordField passwordField;
+    public TextField usernameField;
+
+    public void loginButton(ActionEvent actionEvent) {
+        try {
+            clientSocket = new ClientSocket("localhost", 7500);
+            String answer = clientSocket.connect(usernameField.getText(), passwordField.getText());
+
+            if (answer.equals("OK")) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Connected!");
+                alert.setContentText("Connected to the server");
+                alert.showAndWait().ifPresent(rs -> {
+                    if (rs == ButtonType.OK) {
+                        //TODO: Change the scene
+                    }
+                });
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Wrong credentials");
+                alert.setHeaderText("Wrong credentials were given");
+                alert.setContentText("Either the user doesn't exist or the username/password is wrong.\n Try again");
+                alert.showAndWait();
+                clientSocket.logout();
+            }
+
+        } catch (IOException exception) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("There is no connection to the internet");
+            alert.setHeaderText("Server is either down or there is no internet in this machine");
+            alert.setContentText("Wrong IP address or Port was given.\nOr Server is not open.\n" +
+                    "No internet in this machine.\n Try again");
+            alert.showAndWait();
+        }
+    }
 }
